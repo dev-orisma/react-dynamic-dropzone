3a167b5ee2fa41228414a483cdffcec6c922fb28
Jonathan Widawski <jonathan.widawski@gmail.com>
2016-01-18 17:06:30 +0100

    Fixing open event

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+'use strict';
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var React = require('react');
 var accept = require('attr-accept');
 
 var Dropzone = React.createClass({
+    displayName: 'Dropzone',
 
-    getDefaultProps: function() {
+    getDefaultProps: function getDefaultProps() {
         return {
             disableClick: false,
             disablePaste: false,
@@ -11,11 +16,11 @@ var Dropzone = React.createClass({
         };
     },
 
-    getInitialState: function() {
+    getInitialState: function getInitialState() {
         return {
             isDragActive: false,
             showDropZone: false,
-            showDropZone: false,
+            showDropZone: false
         };
     },
 
@@ -36,30 +41,34 @@ var Dropzone = React.createClass({
         disableClick: React.PropTypes.bool,
         disablePaste: React.PropTypes.bool,
         multiple: React.PropTypes.bool,
-        accept: React.PropTypes.string,
+        accept: React.PropTypes.string
     },
 
-    componentDidMount: function() {
-        window.addEventListener('dragenter',this.onDragEnter);
-        window.addEventListener('dragleave',this.onDragLeave);
-        window.addEventListener('dragover',this.onDragOver);
-        window.addEventListener('drop',this.onDrop);
-        window.addEventListener('paste',this.onPaste);
+    componentDidMount: function componentDidMount() {
+        window.addEventListener('dragenter', this.onDragEnter);
+        window.addEventListener('dragleave', this.onDragLeave);
+        window.addEventListener('dragover', this.onDragOver);
+        window.addEventListener('drop', this.onDrop);
+        window.addEventListener('paste', this.onPaste);
     },
 
-    componentWillUnmount: function() {
+    componentWillUnmount: function componentWillUnmount() {
         window.removeEventListener('dragenter', this.onDragEnter);
-        window.removeEventListener('dragleave',this.onDragLeave);
-        window.removeEventListener('dragover',this.onDragOver);
-        window.removeEventListener('drop',this.onDrop);
-        window.removeEventListener('paste',this.onPaste);
+        window.removeEventListener('dragleave', this.onDragLeave);
+        window.removeEventListener('dragover', this.onDragOver);
+        window.removeEventListener('drop', this.onDrop);
+        window.removeEventListener('paste', this.onPaste);
     },
 
-    allFilesAccepted: function(files) {
-        return files.every(file => accept(file, this.props.accept))
+    allFilesAccepted: function allFilesAccepted(files) {
+        var _this = this;
+
+        return files.every(function (file) {
+            return accept(file, _this.props.accept);
+        });
     },
 
-    onDragEnter: function(event) {
+    onDragEnter: function onDragEnter(event) {
         event.preventDefault();
         // This is tricky. During the drag even the dataTransfer.files is null
         // But Chrome implements some drag store, which is accesible via dataTransfer.items
@@ -72,7 +81,7 @@ var Dropzone = React.createClass({
         this.setState({
             isDragActive: allFilesAccepted,
             isDragReject: !allFilesAccepted,
-            showDropZone: true,
+            showDropZone: true
         });
 
         if (this.props.onDragEnter) {
@@ -80,11 +89,11 @@ var Dropzone = React.createClass({
         }
     },
 
-    onDragOver: function (event) {
+    onDragOver: function onDragOver(event) {
         event.preventDefault();
     },
 
-    onDragLeave: function(event) {
+    onDragLeave: function onDragLeave(event) {
         event.preventDefault();
 
         this.setState({
@@ -92,23 +101,23 @@ var Dropzone = React.createClass({
             isDragReject: false
         });
 
-        if(event.pageX==0){
-            this.setState({showDropZone:false});
+        if (event.pageX == 0) {
+            this.setState({ showDropZone: false });
         }
-        
+
         if (this.props.onDragLeave) {
             this.props.onDragLeave(event);
         }
     },
 
-    onDrop: function(event) {
+    onDrop: function onDrop(event) {
         event.preventDefault();
-        this.captureFile(event)
+        this.captureFile(event);
     },
 
-    onPaste: function(event){
-        if(!this.props.disablePaste){
-            this.setState({showDropZone:true});
+    onPaste: function onPaste(event) {
+        if (!this.props.disablePaste) {
+            this.setState({ showDropZone: true });
             var items = (event.clipboardData || event.originalEvent.clipboardData).items;
             var files = [];
             for (index in items) {
@@ -116,26 +125,26 @@ var Dropzone = React.createClass({
                 if (item.kind === 'file') {
                     var blob = item.getAsFile();
                     blob.lastModifiedDate = new Date();
-                    blob.name = "image-"+new Date()+".jpg";
+                    blob.name = "image-" + new Date() + ".jpg";
                     blob.type = "image/jpeg";
                     var file = new File([blob], blob.name);
                     files.push(file);
                 }
             }
-            if(files.length){
-                this.captureFile(event,files);
+            if (files.length) {
+                this.captureFile(event, files);
             }
         }
     },
 
-    captureFile: function(event, files){
+    captureFile: function captureFile(event, files) {
         this.setState({
             isDragActive: false,
             isDragReject: false,
-            showDropZone: false,
+            showDropZone: false
         });
 
-        var droppedFiles = files?files: (event.dataTransfer ? event.dataTransfer.files : event.target.files);
+        var droppedFiles = files ? files : event.dataTransfer ? event.dataTransfer.files : event.target.files;
         var max = this.props.multiple ? droppedFiles.length : 1;
         var files = [];
 
@@ -153,31 +162,35 @@ var Dropzone = React.createClass({
             if (this.props.onDropAccepted) {
                 this.props.onDropAccepted(files, event);
             }
-        }
-        else {
+        } else {
             if (this.props.onDropRejected) {
                 this.props.onDropRejected(files, event);
             }
         }
-
     },
 
-    onClick: function () {
+    onClick: function onClick() {
         if (!this.props.disableClick) {
             this.open();
         }
     },
 
-    open: function() {
+    open: function open() {
         var fileInput = React.findDOMNode(this.refs.fileInput);
         fileInput.value = null;
         fileInput.click();
     },
 
-    render: function() {
+    render: function render() {
 
-        if(!this.state.showDropZone){
-            return <span/>;
+        if (!this.state.showDropZone) {
+            return React.createElement('input', {
+                    type: 'file',
+                    ref: 'fileInput',
+                    style: { display: 'none' },
+                    multiple: this.props.multiple,
+                    accept: this.props.accept,
+                    onChange: this.onDrop })
         }
 
         var className;
@@ -199,61 +212,54 @@ var Dropzone = React.createClass({
             if (this.props.activeStyle) {
                 activeStyle = this.props.activeStyle;
             }
-        } 
-        else if (!className) {
+        } else if (!className) {
             style = {
                 position: 'fixed',
-                width:'100%',
-                height:'100%',
+                width: '100%',
+                height: '100%',
                 zIndex: '9999',
-                'backgroundColor': 'rgba(0, 0, 0, 0.8)',        
-                'color':'white',
+                'backgroundColor': 'rgba(0, 0, 0, 0.8)',
+                'color': 'white',
                 fontSize: '40px',
                 padding: '200px',
                 textAlign: 'center',
                 top: '0',
                 bottom: '0',
                 right: '0',
-                left: '0',
+                left: '0'
 
             };
             activeStyle = {
-                'backgroundColor': 'white',        
-                'color':'black',
+                'backgroundColor': 'white',
+                'color': 'black'
             };
         }
 
         var appliedStyle;
         if (activeStyle && this.state.isDragActive) {
-            appliedStyle = {
-                ...style,
-                ...activeStyle
-                };
-            } 
-            else {
-                appliedStyle = {
-                ...style
-            };
+            appliedStyle = _extends({}, style, activeStyle);
+        } else {
+            appliedStyle = _extends({}, style);
         };
 
-        return (
-            <div
-                className={className}
-                style={appliedStyle}
-                onClick={this.onClick}
-                onDragEnter={this.onDragEnter}
-                onDragOver={this.onDragOver}
-                onDragLeave={this.onDragLeave}
-                onDrop={this.onDrop}>
-                    {this.props.children}
-                    <input
-                        type='file'
-                        ref='fileInput'
-                        style={{ display: 'none' }}
-                        multiple={this.props.multiple}
-                        accept={this.props.accept}
-                        onChange={this.onDrop}/>
-            </div>
+        return React.createElement(
+            'div',
+            {
+                className: className,
+                style: appliedStyle,
+                onClick: this.onClick,
+                onDragEnter: this.onDragEnter,
+                onDragOver: this.onDragOver,
+                onDragLeave: this.onDragLeave,
+                onDrop: this.onDrop },
+            this.props.children,
+            React.createElement('input', {
+                type: 'file',
+                ref: 'fileInput',
+                style: { display: 'none' },
+                multiple: this.props.multiple,
+                accept: this.props.accept,
+                onChange: this.onDrop })
         );
     }
 });
